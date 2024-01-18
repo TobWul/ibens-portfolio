41aea3ec919b704cf48d5d38a1634a75f7a330ee
Tobias Wulvik <tobias@Tobiass-MacBook-Pro.local>
2024-01-18 21:11:00 +0100

    changing layout on portfolio page

--- web/pages/portfolio.tsx
+++ web/pages/portfolio.tsx
@@ -15,51 +15,41 @@ const PortfolioPage = ({
   description: BlockContent;
   rows: { columns: Project[] }[];
 }) => {
+  const projects = rows.map(({ columns }) => columns).flat();
+
   return (
     <main>
-      <div>
-        {rows.map(({ columns }, i) => (
-          <div
-            key={i}
-            className="md:grid"
-            style={{
-              gridTemplateColumns: `repeat(${
-                columns.length + (i === 0 ? 1 : 0)
-              }, 1fr)`,
-            }}
-          >
-            {i === 0 && (
-              <div className="p-24 w-full block [&+&]:border-l border-b">
-                <h1 className="text-[128px] font-[300]">{title}</h1>
-                <PortableText blocks={description} />
-                <div className="mt-24">
-                  <TextLink href="/" icon="rightArrow">
-                    Se min CV
-                  </TextLink>
-                </div>
-              </div>
-            )}
-            {columns.map((project) => (
-              <Link
-                href={"/" + project.slug?.current}
-                key={project._id}
-                className="p-24 w-full flex flex-col h-full [&+&]:border-l border-b group transition-colors duration-700 ease-out"
-              >
-                <Image
-                  image={project.mainImage}
-                  alt={project.title}
-                  className="flex-1 overflow-hidden transition-all duration-[1s] ease-out group-hover:grayscale"
-                  aspectRatio={1}
-                />
-
-                <div className="mt-16">
-                  <h1>{project.title}</h1>
-                  <p>{project.subtitle}</p>
-                </div>
-              </Link>
-            ))}
+      <div className="p-24 md:p-64">
+        <div className="w-full block mb-64">
+          <h1 className="text-[128px] font-[300]">{title}</h1>
+          <PortableText blocks={description} className="max-w-3xl" />
+          <div className="mt-24">
+            <TextLink href="/" icon="rightArrow">
+              Se min CV
+            </TextLink>
           </div>
-        ))}
+        </div>
+        <div className="md:grid grid-cols-3 gap-32">
+          {projects.map((project) => (
+            <Link
+              href={"/" + project.slug?.current}
+              key={project._id}
+              className="w-full flex flex-col h-full group transition-colors duration-700 ease-out"
+            >
+              <Image
+                image={project.mainImage}
+                alt={project.title}
+                className="flex-1 overflow-hidden transition-all duration-[1s] ease-out group-hover:grayscale"
+                aspectRatio={1}
+              />
+
+              <div className="mt-16">
+                <h1>{project.title}</h1>
+                <p>{project.subtitle}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </main>
   );
